19a22e3814dbfce55af7e51f6ab14917ddde5ed5
Ivan Lopes <lopesivan.ufrj@gmail.com>
2022-12-15 23:12:24 -0300

    Adiciona o arquivo 01.ex/01/.gitignore

---
+++ 01.ex/01/hellworld.cs
@@ -0,0 +1,14 @@
+using System;
+
+namespace HelloWorld
+{
+    class Hello
+    {
+        [STAThread]
+        static void Main(string[] args)
+        {
+            Console.Out.WriteLine("Hello, world!");
+        }
+    }
+}
+
